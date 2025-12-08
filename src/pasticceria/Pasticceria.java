14e9c2c477a7faf43e2768ca8d3c0a10b46dcc93
elena <lin.elena@franchettisalviani.net>
2025-12-08 23:32:35 +0100

    ver1.0

---
+++ src/pasticceria/Pasticceria.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pasticceria;
+
+/**
+ *
+ * @author zxt02
+ */
+public class Pasticceria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Form1 f = new Form1();
+        f.setSize(1000, 600);
+        f.setVisible(true);
+    }
+    
+}
